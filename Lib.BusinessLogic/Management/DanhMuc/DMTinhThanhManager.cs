b71829c1e28b286b50360d9eb7060b8fafea5804
LeKhanh <khanhld0211@gmail.com>
2022-10-21 17:32:03 +0700

    ahsd

--- Lib.BusinessLogic/Management/DanhMuc/DMTinhThanhManager.cs
+++ Lib.BusinessLogic/Management/DanhMuc/DMTinhThanhManager.cs
@@ -28,5 +28,33 @@ namespace Lib.BusinessLogic.Management
                 return vResult;
             }
         }
+
+      /*  public override DMTinhThanhModel GetById(Guid id)
+        {
+            using (BaseData db = new BaseData(_nameConnStr))
+            {
+                var vResult = db.Query<DMTinhThanhModel>(@"select x.*, c1.Ten TenCap1, c1.Url UrlCap1, c2.Ten TenCap2, c2.Url UrlCap2 from DMLoaiSanPham x left join DMLoaiSanPham c1 on x.IdCap1 = c1.Id
+                        left join DMLoaiSanPham c2 on x.IdCap2 = c2.Id where x.Id = @id"
+                    , new { id = id }).FirstOrDefault();
+                return vResult;
+            }
+        }*/
+     
+        public IEnumerable<DMTinhThanhModel> SelectAll()
+        {
+            using (BaseData db = new BaseData(_nameConnStr))
+            {
+                var vResult = db.Query<DMTinhThanhModel>(@"select x.* from DMTinhThanh x where x.Cap = 1 order by UuTien");
+              /*  foreach (var cap1 in vResult)
+                {
+                    cap1.DanhMucCon = db.Query<DMTinhThanhModel>(@"select x.* from DMLoaiSanPham x where x.Cap = 2 and x.Active = 1 and x.IdCap1 = @Id order by UuTien", new { cap1.Id });
+                    foreach (var cap2 in cap1.DanhMucCon)
+                    {
+                        cap2.DanhMucCon = db.Query<DMTinhThanhModel>(@"select x.* from DMLoaiSanPham x where x.Cap = 3 and x.Active = 1 and x.IdCap2 = @Id order by UuTien", new { cap2.Id });
+                    }
+                }*/
+                return vResult;
+            }
+        }
     }
 }
--- Lib.BusinessLogic/Management/DatSach/DMPhongGDManager.cs
+++ Lib.BusinessLogic/Management/DatSach/DMPhongGDManager.cs
@@ -11,8 +11,17 @@ namespace Lib.BusinessLogic.Management
 {
     public class DMPhongGDManager : BaseManager<DMPhongGDManager, DMPhongGD, DMPhongGDModel>
     {
-
-        public IEnumerable<DMPhongGDModel> SelectDataByPage(string sSearch,  Guid? idTinhThanh,
+        public override DMPhongGDModel GetById(Guid id)
+        {
+            using (BaseData db = new BaseData(_nameConnStr))
+            {
+                var vResult = db.Query<DMPhongGDModel>(@"select x.*, l.Ten TinhThanh from DMPhongGD x join DMTinhThanh l on x.IdTinhThanh = l.Id where x.Id = @id"
+                    , new { id = id }).FirstOrDefault();
+                return vResult;
+            }
+        }
+     
+        public IEnumerable<DMPhongGDModel> SelectDataByPage(string sSearch, Guid? idTinhThanh,
            DateTime? dateFrom, DateTime? dateTo, int dcPageIndex, int dcPageItem, out int total, string sOrder = null)
         {
             total = 0;
@@ -45,8 +54,8 @@ namespace Lib.BusinessLogic.Management
                     vParams.Add("idTinhThanh", idTinhThanh.Value);
                     strWhere += " and (x.IdTinhThanh = @idTinhThanh or x.IdTinhThanh in (select l.Id from DMTinhThanh l where l.Id = @idTinhThanh)) ";
                 }
-                
-               // string strQurey = @"select * INTO #dataList from DMPhongGD x where 1 = 1 " + strWhere;
+
+                // string strQurey = @"select * INTO #dataList from DMPhongGD x where 1 = 1 " + strWhere;
 
                 string strQurey = @"select x.*, r.[Ten] TenTT from DMTinhThanh x join DMPhongGD r on x.IdTinhThanh = r.Id where 1 = 1 " + strWhere;
 
@@ -55,21 +64,6 @@ namespace Lib.BusinessLogic.Management
                     strQurey = strQurey + @" order by " + sOrder;
                 }
 
-
-
-               /* strQurey = strQurey + @"
-
-                select count(id) Total from #dataList
-
-                select x.Id, x.TenPGD, x.IdTinhThanh, x.DiaChi, x.Email, x.TaiKhoan, x.MatKhau, x.MatKhauMacDinh, x.NgayTao, x.StrSearch,
-                
-                tt.Ten TinhThanh, dm.TenCap1 DanhMucCap1, dm.TenCap2 DanhMucCap2 from #dataList x left join (select l.*, c1.Ten TenCap1, c2.Ten TenCap2 from DMLoaiSanPham l left join DMLoaiSanPham c1 on l.IdCap1 = c1.Id
-                left join DMLoaiSanPham c2 on l.IdCap2 = c2.Id) dm on x.IdTinhThanh = dm.Id
-                order by " + (sOrder.IsNotNullOrEmpty() ? sOrder : "x.NgayTao desc") + @" 
-                OFFSET @dcPageItem * (@dcPageIndex - 1) ROWS
-                FETCH NEXT @dcPageItem ROWS ONLY OPTION (RECOMPILE)
-
-                drop table #dataList";*/
                 if (dcPageItem > 10000)
                 {
                     dcPageItem = 10000;
@@ -81,6 +75,6 @@ namespace Lib.BusinessLogic.Management
                 return db.QueryAndTotal<DMPhongGDModel>(strQurey, out total, vParams);
             }
         }
-       
+
     }
 }
